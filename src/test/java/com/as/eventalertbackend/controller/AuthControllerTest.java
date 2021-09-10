d8d8ccee3f1dc344ee13b9b8d8ded8e51900ce2d
Adrian Scarlatescu <scarlatescu.m.a@gmail.com>
2021-09-10 17:22:01 +0300

    Remove field already inherited

--- src/test/java/com/as/eventalertbackend/controller/AuthControllerTest.java
+++ src/test/java/com/as/eventalertbackend/controller/AuthControllerTest.java
@@ -6,18 +6,9 @@ import com.as.eventalertbackend.controller.response.AuthRefreshTokenResponse;
 import com.as.eventalertbackend.controller.response.AuthTokensResponse;
 import com.as.eventalertbackend.data.model.User;
 import com.as.eventalertbackend.service.AuthService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
@@ -25,16 +16,12 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = AuthController.class)
 class AuthControllerTest extends AbstractControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
 
     @MockBean
     private AuthService authService;
