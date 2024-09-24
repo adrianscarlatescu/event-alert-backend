f10db7f011b2bf4a761dc645ef544ae7d19bc093
Adrian Scarlatescu <scarlatescu.m.a@gmail.com>
2024-09-24 17:08:36 +0300

    Log entry point endpoint

--- src/main/java/com/as/eventalertbackend/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/as/eventalertbackend/security/jwt/AuthEntryPointJwt.java
@@ -15,7 +15,7 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException {
 
-        log.error("Unauthorized error: {}", authException.getMessage());
+        log.error("Unauthorized error: {}, endpoint: {}", authException.getMessage(), request.getRequestURI());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
 
     }
